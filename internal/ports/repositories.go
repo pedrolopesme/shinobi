586eabf20e6947675f2c5235a27642a6b1ea6648
pedro.mendes <pedro.mendes@g.globo>
2021-12-11 19:30:40 -0300

    minor: add moving average to quotes repo interface

--- internal/ports/repositories.go
+++ internal/ports/repositories.go
@@ -4,4 +4,5 @@ import "github.com/pedrolopesme/shinobi/internal/domain"
 
 type QuotesRepositoryContract interface {
 	GetQuotes(symbol string) ([]domain.Quote, error)
+	GetMovingAveragePeriod(symbol string, period int) (float32, error)
 }
